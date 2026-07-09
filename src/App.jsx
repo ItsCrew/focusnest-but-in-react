b3c240120c380e255b085bfad413560100dc3c80
Crew <aryanchanana20@gmail.com>
2026-07-10 01:44:43 +0530

    Coming soon page complete (to be changed later)

--- src/App.jsx
+++ src/App.jsx
@@ -5,24 +5,20 @@ import AppShell from './Components/Global/AppShell'
 import { Route, Routes } from 'react-router-dom'
 import Home from './Pages/Home'
 import Tasks from './Pages/Tasks'
+import Soon from './Pages/Soon'
 
 const App = () => {
   return (
     <div className='min-h-screen w-full bg-background-dark text-font-dark'>
-    <Routes>
-      <Route
-            path="/"
-            element={
-              <>
-                <Page />
-              </>
-            }
-          />
-      <Route element={<AppShell />}>
-        <Route path='/home' element={<Home/>}/>
-        <Route path='/tasks' element={<Tasks/>}/>
-      </Route>
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Page />}/>
+        <Route element={<AppShell />}>
+          <Route path='/home' element={<Home />} />
+          <Route path='/tasks' element={<Tasks />} />
+          <Route path='/notes' element={<Soon />} />
+        </Route>
+
+      </Routes>
     </div>
   )
 }
---
+++ src/Pages/Soon.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Soon = () => {
+  return (
+    <div className='flex justify-center items-center text-8xl h-screen'>
+        Coming Soon!
+    </div>
+  )
+}
+
+export default Soon
