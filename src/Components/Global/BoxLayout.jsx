f7940716f2a4dbc8775790264852458d79c8a59a
Crew <68228746+ItsCrew@users.noreply.github.com>
2026-07-31 05:20:10 +0530

    Passed classname to Boxlayout

--- src/Components/Global/BoxLayout.jsx
+++ src/Components/Global/BoxLayout.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
-const BoxLayout = ({title, children}) => {
-
+const BoxLayout = ({ title, children, className = "" }) => {
   return (
     <div className="space-y-2">
-      <p className="text-3xl font-bold text-white tracking-tight">{title}</p>
-      <div className='dark:bg-tasksbox-bg h-auto min-h-137.5 w-[58vw] border-2 border-headings-dark rounded-[30px] p-6 flex flex-col justify-center'>
+      {title && <p className="text-3xl font-bold text-white tracking-
+  tight">{title}</p>}
+
+      <div className={`dark:bg-tasksbox-bg h-auto min-h-137.5 w-[58vw] border-2
+  border-headings-dark rounded-[30px] p-6 flex flex-col justify-center ${className}`}>
         {children}
       </div>
     </div>
--- src/Components/Settings/SettingsBox.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const SettingsBox = () => {
-  return (
-    <div>SettingsBox</div>
-  )
-}
-
-export default SettingsBox
