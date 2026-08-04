35ffe7385fce381219efd0d58be585491ae40472
Crew <68228746+ItsCrew@users.noreply.github.com>
2026-08-04 12:19:33 +0530

    Fixed About page button

--- src/Components/About/Intro.jsx
+++ src/Components/About/Intro.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { MoveRight } from 'lucide-react'
+import { Link, Links } from 'react-router-dom'
+
 
 const Intro = () => {
     return (
@@ -9,7 +11,9 @@ const Intro = () => {
             <p className=' text-xl'>FocusNest is your ultimate productivity companion, built around the proven Pomodoro Technique. Whether you're studying for exams, coding your next project, or tackling important work tasks — FocusNest creates the perfect environment to maximize your focus and maintain sustainable productivity.</p>
             <p className=' text-xl'>Our intelligent timer automatically manages your work-break cycles, so you can stop watching the clock and start achieving your goals. With smart task management and personalized customization, FocusNest adapts to your unique workflow.</p>
             </div>
-            <button className='bg-headings-dark flex justify-center gap-1 h-11 items-center text-lg p-5 rounded-xl text-background-dark hover:text-headings-dark hover:bg-navbg-dark cursor-pointer hover:border-2 hover:border-headings-dark transition-all duration-300 ease shadow-[0_4px_12px_rgba(107,143,113,0.3)] hover:shadow-[0_6px_20px_rgba(107,143,113,0.4)]' >Start Being Productive!<MoveRight /></button>
+            <Link to='/Home'>
+                <button className='bg-headings-dark flex justify-center gap-1 h-11 items-center text-lg p-5 rounded-xl text-background-dark hover:text-headings-dark hover:bg-navbg-dark cursor-pointer hover:border-2 hover:border-headings-dark transition-all duration-300 ease shadow-[0_4px_12px_rgba(107,143,113,0.3)] hover:shadow-[0_6px_20px_rgba(107,143,113,0.4)]' >Start Being Productive!<MoveRight /></button>
+            </Link>
         </div>
     )
 }
