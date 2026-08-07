0613f522f18cf7d62b12234b7cb31cb66c6ab3f5
Crew <68228746+ItsCrew@users.noreply.github.com>
2026-08-07 19:25:02 +0530

    Button Selection Logic added

--- frontend/src/Components/Home/TimerNav.jsx
+++ frontend/src/Components/Home/TimerNav.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
+import { useState } from 'react'
 
-const Timer = () => {
+const Timer = ({activeButton, handleMode}) => {
+
   return (
     <div className='flex justify-center items-center'>
       <div className='flex justify-center items-center gap-3 font-medium'>
-        <button className='bg-white text-black h-13.75 w-30 rounded-2xl cursor-pointer hover:-translate-y-1.25 transition-all duration-300 ease'>Pomodoro</button>
-        <button className='bg-white text-black h-13.75 w-30 rounded-2xl cursor-pointer hover:-translate-y-1.25 transition-all duration-300 ease'>Short Break</button>
-        <button className='bg-white text-black h-13.75 w-30 rounded-2xl cursor-pointer hover:-translate-y-1.25 transition-all duration-300 ease'>Long Break</button>
+        <button onClick={()=> { handleMode(1)}
+        } className={`${activeButton === 1 ? 'bg-buttonsfocus-bg' : 'bg-white'} text-black h-13.75 w-30 rounded-2xl cursor-pointer hover:-translate-y-1.25 transition-all duration-300 ease`}>Pomodoro</button>
+        <button onClick={()=> { handleMode(2)}
+        } className={`${activeButton === 2 ? 'bg-buttonsfocus-bg' : 'bg-white'} text-black h-13.75 w-30 rounded-2xl cursor-pointer hover:-translate-y-1.25 transition-all duration-300 ease`}>Short Break</button>
+        <button onClick={()=> { handleMode(3)}
+        } className={`${activeButton === 3 ? 'bg-buttonsfocus-bg' : 'bg-white'} text-black h-13.75 w-30 rounded-2xl cursor-pointer hover:-translate-y-1.25 transition-all duration-300 ease`}>Long Break</button>
       </div>
     </div>
   )
--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,14 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Timer from '../Components/Home/TimerNav'
 import TimerDisplay from '../Components/Home/TimerDisplay'
 import ActiveTask from '../Components/Home/ActiveTask'
 import TimerControls from '../Components/Home/TimerControls'
 
 const Home = () => {
+
+  const [activeButton, setactiveButton] = useState(null)
+  const [timeLeft, setTimeLeft] = useState(25 * 60);
+
+const handleMode = (buttonid) => {
+  setactiveButton(buttonid)
+}
+
+
   return (
     <div className='min-h-screen flex flex-col justify-center items-center'>
       <div className='flex flex-col items-center'>
-        <Timer/>
+        <Timer activeButton={activeButton} handleMode={handleMode}/>
         <TimerDisplay/>
         <ActiveTask/>
         <TimerControls/>
